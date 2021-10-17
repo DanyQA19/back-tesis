f65aa760634520e122f2862ce48723b743174679
Dany <dany4ac@gmail.com>
2021-10-17 11:47:14 -0500

    agregando comentario

--- src/models/Indicendia.js
+++ src/models/Indicendia.js
@@ -6,7 +6,7 @@ const incidenciaSchema = new Schema ({
     responsable: {type: Schema.ObjectId, ref: 'usuario'}, // se hace referencia al trabajador de soporte
     tipo: {type: String, required: true},          // SISTEMA DE GESTIÓN DOCUMENTAL o MÓDULO DE ATENCIÓN DE TRÁMITES        
     fechaInicio: {type: Date, required: true, default: Date.now},
-    fechaTermino: {type: Date, required: true, default: Date.now},
+    fechaTermino: {type: Date, required: true, default: Date.now}, // se agrega al final
     descripcion: {type: String, maxlength: 255},
     nombreCliente: {type: String, maxlength: 30},
     prioridad: {type: String, default: 'Bajo'},
